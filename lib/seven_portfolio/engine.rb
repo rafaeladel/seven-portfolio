069f06a05a21872cc9dc0a1be5c98ba482bc1eb2
rafael <rafael.adel20@gmail.com>
2015-06-12 17:58:29 +0200

    remove require

--- lib/seven_portfolio/engine.rb
+++ lib/seven_portfolio/engine.rb
@@ -1,10 +1,10 @@
 require 'jquery-rails'
 require 'bootstrap-sass'
 require 'minitest/autorun'
-require 'mini_backtrace'
+#require 'mini_backtrace'
 require 'seven_gallery'
 require 'auto_html'
-require 'factory_girl'
+#require 'factory_girl'
 
 module SevenPortfolio
   class Engine < ::Rails::Engine
