0f26eaebc6fd27d2bc874e8578bd5a68fbccd955
rafael <rafael.adel20@gmail.com>
2015-06-27 17:05:34 +0200

    fix

--- app/models/seven_portfolio/concerns/item.rb
+++ app/models/seven_portfolio/concerns/item.rb
@@ -1,21 +1,33 @@
 module SevenPortfolio::Concerns::Item
   extend ActiveSupport::Concern
   included do
+    enum item_type: [:no_media, :gallery, :video]
     has_one :item_video, class_name:'SevenPortfolio::ItemVideo', foreign_key: "seven_portfolio_item_id"
     has_one :item_gallery, class_name: 'SevenGallery::Gallery', foreign_key: "seven_portfolio_item_id"
-    accepts_nested_attributes_for :item_video, :item_gallery
+    accepts_nested_attributes_for :item_video, :item_gallery, reject_if: :all_blank
 
     before_save :process_type
   end
 
   def process_type
-    self.build_item_video if self.item_type == 0
-    self.build_item_gallery if self.item_type == 1
+    if no_media?
+      nil
+    elsif gallery?
+      build_item_gallery
+    elsif video?
+      build_item_video
+    end
   end
 
   def type_content
-    self.item_gallery if self.item_type == 0
-    self.item_video if self.item_type == 1
+    if no_media?
+      nil
+    elsif gallery?
+      item_gallery
+    elsif video?
+      item_video
+    end
   end
+
 
 end
